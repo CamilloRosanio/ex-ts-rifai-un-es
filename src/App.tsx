137ba26185881924fe76e8526b73587f83bf695f
MyUsername <rosanio.c@gmail.com>
2025-04-23 09:40:48 +0200

    Funzione FETCH di supporto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-function App() {
-
-
-  return (
-    <>
-      App content
-    </>
-  )
+// Funzione FETCH di supporto
+/* Per prima cosa definisco la mia funzione di supporto per la Fetch.
+Siccome non so che tipo di dati riceverò ed in ogni caso non posso ricordare o elencare tutte le KEYS dell'OBJECT che ricevo, procedo specificando che la risposta sarà di tipo UNKNOWN. Dato che tutte le risposte alle fetch contengono la KEY "ok", mi accerto prima di tutto che la richiesta vada a buon fine, o lancio un ERROR.
+*/
+async function fetchJson(url: string): Promise<unknown> {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Errore nella fetch: ${response.status} ${response.statusText}`);
+  }
+  const data = await response.json();
+  return data;
 }
 
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Il "!" si chiama ASSERTION OPERATOR
+// Serve a comunicare a TYPESCRIPT che l'elemento non è NULL.
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
